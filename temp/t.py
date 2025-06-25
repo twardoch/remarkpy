2b911832d3871d021be4db5e9f06a0219fcc7401
Adam Twardoch <adam+github@twardoch.com>
2025-06-25 23:17:11 +0200

    Auto-merge: Resolved conflicts

--- temp/t.py
+++ temp/t.py
@@ -1,8 +1,9 @@
 import json
+
 import quickjs
 
-msg = {'amesg': {'bmesg': [1, 2, 3, 4]}}
+msg = {"amesg": {"bmesg": [1, 2, 3, 4]}}
 
-with open('./bundle.js') as f:
-    _fn = quickjs.Function('make_schema', f.read())
+with open("./bundle.js") as f:
+    _fn = quickjs.Function("make_schema", f.read())
     print(json.dumps(_fn(msg), indent=2))
